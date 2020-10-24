97e03a91a236f9b783ae482a898f1fe88e48b289
taniya <tptanupat@gmail.com>
2020-10-24 18:45:36 +0530

    Platform Added

--- sketch.js
+++ sketch.js
@@ -11,7 +11,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+	platform = new Ground(150,305300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
